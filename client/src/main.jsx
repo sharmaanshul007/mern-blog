2f0e1e445cedf0f458970fb38e3c40fe5c37a23e
sharmaanshul007 <sasansanshul@gmail.com>
2024-10-28 02:11:09 +0530

    Header file created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import Header from './components/Header.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StrictMode>
+    <Header></Header>
     <App />
   </StrictMode>
   </BrowserRouter>
