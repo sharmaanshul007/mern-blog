4a510378a419704b48e42747491d7761f5b93f2e
sharmaanshul007 <sasansanshul@gmail.com>
2024-10-31 18:13:49 +0530

    added dashboard sidebar functionality

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {HiArrowSmRight, HiUser} from 'react-icons/hi';
+import {Sidebar} from 'flowbite-react';
+import { Link, useLocation,useSearchParams} from 'react-router-dom';
+import { useState , useEffect} from 'react';
+
+export default function DashSidebar() {
+    const location = useLocation();
+  const [searchParams,setSearchParams] = useSearchParams();
+  const [tab,setTab] = useState("");
+  useEffect(()=>{
+    const tabFromUrl = searchParams.get('tab');
+    if(tabFromUrl){
+      setTab(tabFromUrl);
+    }
+  },[location.search])
+  return (
+    <Sidebar classname='w-full md:w-56'>
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+                <Link to='/dashboard?tab=profile'>
+                <Sidebar.Item active = {tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark'>
+                    Profile
+                </Sidebar.Item></Link>
+                <Sidebar.Item active icon={HiArrowSmRight} classname='cursor-pointer'>
+                    Sign Out
+                </Sidebar.Item>
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import {useLocation, useSearchParams} from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 export default function Dashboard() {
+  const location = useLocation();
+  const [searchParams,setSearchParams] = useSearchParams();
+  const [tab,setTab] = useState("");
+  useEffect(()=>{
+    const tabFromUrl = searchParams.get('tab');
+    if(tabFromUrl){
+      setTab(tabFromUrl);
+    }
+    console.log(tabFromUrl);
+  },[location.search])
   return (
-    <div>
-      Hello DashBoard
+    <div className='min-h-screen flex flex-col sm:flex-row'>
+      {/* SideBar */}
+      <div className=''>
+        <DashSidebar></DashSidebar>
+      </div>
+
+      <div>
+        {
+          tab === 'profile' && (<DashProfile></DashProfile>) 
+        }
+      </div>
     </div>
   )
 }
