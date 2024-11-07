8a1565395be9332635208ae23214c3c5cb50a3a7
sharmaanshul007 <sasansanshul@gmail.com>
2024-11-07 14:05:44 +0530

    Added the route for getposts

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,6 +1,9 @@
 import Post from "../models/post.model.js";
 import {errorHandler} from '../utils/error.js';
 export const create = async(req,res,next) => {
+    console.log("This is request by abhinav the buwade",req);
+    console.log("buwade ka aashiq");
+    console.log(req.user);
     if(!req.user.isAdmin){
         return next(errorHandler(401,"Unauthorized User bhai "));
     }
@@ -24,5 +27,47 @@ export const create = async(req,res,next) => {
         res.status(201).json(savedPost);
     }catch(err){
         return next(errorHandler(400,err));
+    }
+}
+
+export const getposts = async(req,res,next) => {
+    try{
+        const startIndex = parseInt(req.query.startIndex || 0);
+        const limit = parseInt(req.query.limit || 9);
+        const sortDirection = req.query.order === "asc" ? 1 : -1;
+        const posts = await Post.find({
+            ...(req.query.userId && {userId:req.query.userId}),
+            ...(req.query.category && {category:req.query.category}),
+            ...(req.query.slug && {slug:req.query.slug}),
+            ...(req.query.postId && {_id:req.query.slug}),
+            ...(req.query.searchItem && {
+                $or:[
+                    {title:{$regex:req.query.searchItem,$options:'i'}},
+                    {content:{$regex:req.query.searchItem,$options:'i'}},
+
+                ]
+            }),
+
+        }).sort({updatedAt:sortDirection}).skip(startIndex).limit(limit)
+        const totalPosts = await Post.countDocuments();
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        )
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt:{$gte:oneMonthAgo},
+        })
+
+        res.status(200).json({
+            posts,
+            lastMonthPosts,
+            totalPosts,
+        })
+
+    }catch(error){
+        console.log(error);
+        next(errorHandler(400,error));
     }
 }
