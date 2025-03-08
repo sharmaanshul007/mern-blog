f4a80c5856134b6ca2eeb949b267744ac8786540
sharmaanshul007 <sasansanshul@gmail.com>
2025-03-08 13:31:38 +0530

    added changes to tailwind to remove require error

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,21 @@
+// /** @type {import('tailwindcss').Config} */
+// const flowbite = require("flowbite-react/tailwind");
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     flowbite.content(),
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [flowbite.plugin(),
+//     require('tailwind-scrollbar'),
+//   ],
+// }
+
 /** @type {import('tailwindcss').Config} */
-const flowbite = require("flowbite-react/tailwind");
+import flowbite from "flowbite-react/tailwind";
 export default {
   content: [
     "./index.html",
